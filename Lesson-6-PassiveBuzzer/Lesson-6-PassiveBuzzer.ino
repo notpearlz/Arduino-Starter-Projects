e8a901f7b6a34b4f546526a44be540dbcf0110d9
Bun <lolmebunbun@gmail.com>
2025-08-14 12:22:16 -0500

    Leson-5-Buzzers

---
+++ Lesson-6-PassiveBuzzer/Lesson-6-PassiveBuzzer.ino
@@ -0,0 +1,15 @@
+int buzzer = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(buzzer, HIGH);
+  delay(1000);
+  digitalWrite(buzzer, LOW);
+  delay(1000);
+
+}
